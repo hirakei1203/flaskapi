60bbbdd53fc8805fa0c59e61387e2acb8cde1a9a
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-12-05 10:41:34 +0900

    try2

--- server.py
+++ server.py
@@ -46,6 +46,6 @@ def result():
 if __name__ == '__main__':
     load_model()
     app.debug = True
-#     app.run(host='localhost', port=5000)
+    app.run(host='localhost', port=5000)
 #     デプロイのため、パスを変更してみました。。。
-    app.run(host='0.0.0.0', port=5000)
+#     app.run(host='0.0.0.0', port=5000)
